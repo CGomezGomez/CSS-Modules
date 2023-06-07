00a1fe3d4c40dcdebdbff4f68c6006ffee1f4d70
CGomezGomez <cggomez93@gmail.com>
2023-06-07 12:25:15 +0200

    CSS-Modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,33 @@ import Card from "./components/Card/Card";
 const App = () => {
 
 	return (
-		<>
-			<Card />
-		</>
+		<div class="container">
+			<Card 
+				src='/icon-sedans.svg'
+				alt='Sedans'
+				color='orange'
+				name='SEDANS'
+				text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+				bcolor='buttonorange'
+			/>
+			<Card 
+				src='/icon-suvs.svg'
+				alt='Suvs'
+				color='blue'
+				name='SUVS'
+				text='Take an SUV for its spacious interior ,power m and versality. Perfect for your next family vacation and off-road adventures.'
+				bcolor='buttonblue'
+			/>
+			<Card 
+				src='/icon-luxury.svg'
+				alt='Luxury'
+				color='green'
+				name='LUXURY'
+				text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+				bcolor='buttongreen'
+			/>
+
+		</div>
 
 	)
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,17 @@
 import style from './card.module.css';
 
-const Card = () => {
+const Card = ({src, alt , color , name , text , bcolor }) => {
   return (
 
-    <div className={`${style.container}`}>
-      <div className={`${style.card} ${style.orange}`}>
-          <img src="icon-sedans.svg" alt="Sedans"/>
-          <h2 className={style.cardh2}>SEDANS</h2>
-          <p className={style.cardp}>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-          <button className={`${style.button} ${style.buttonorange}`}>Learn More</button>
+      <div className={`${style.card} ${style[color]}`}>
+          <img className={style.cardimg} src={src} alt={alt} />
+          <h2 className={style.cardh2}>{name}</h2>
+          <p className={style.cardp}>{text}</p>
+          <button className={`${style.button} ${style[bcolor]}`}>Learn More</button>
       </div>
-      <div className={`${style.card} ${style.blue}`}>
-          <img src="icon-suvs.svg" alt="Suvs"/>
-          <h2 className={style.cardh2}>SUVS</h2>
-          <p className={style.cardp}>Take an SUV fpr ots spacious interior, power, and versality. Perfect for your next family vacation and off-road adventures.</p>
-          <button className={`${style.button} ${style.buttonblue}`}>Learn More</button>
-      </div>
-      <div className={`${style.card} ${style.green}`}>
-          <img src="icon-luxury.svg" alt="Luxury"/>
-          <h2 className={style.cardh2}>LUXURY</h2>
-          <p className={style.cardp}>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
-          <button className={`${style.button} ${style.buttongreen}`}>Learn More</button>
-      </div>
-    </div>
     
   );
 };
+
 
 export default Card;
